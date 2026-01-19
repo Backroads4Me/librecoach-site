5b6d61763104c3025f0190fe3381b22dc891b0c0
Ted Lanham <tedlanham@gmail.com>
2026-01-18 23:12:18 -0500

    Update favicon and logo assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Libre Coach",
-      favicon: "/favicon-32x32.png",
+      favicon: "/favicon-64x64.png",
       logo: {
         src: "./src/assets/logo.svg",
         replacesTitle: true,
